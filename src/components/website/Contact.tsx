2b6606b0ef0c70efdfbd579b2fdf064a60467179
Z User <z@container>
2026-06-21 18:04:10 +0000

    Update Google Maps to Goodness Filling Station location

--- src/components/website/Contact.tsx
+++ src/components/website/Contact.tsx
@@ -97,7 +97,7 @@ export function Contact() {
           {/* Google Maps */}
           <div className="flex flex-col items-center justify-center overflow-hidden rounded-2xl">
             <iframe
-              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=Dowhenya+Street+Community+25+Container+Junction+Behind+The+Noble+Restaurant+Ghana"
+              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=Goodness+Filling+Station+Savannah+Junction+Community+25+Dowhenya+Street+Behind+The+Noble+Restaurant+Ghana"
               width="100%"
               height="100%"
               style={{ border: 0, minHeight: '320px' }}
