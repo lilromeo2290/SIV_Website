41a107ff5d28938256e02a6b809f6e05b7690b96
Z User <z@container>
2026-08-12 11:26:45 +0000

    154f8191-5e5a-46f7-83d4-d097f0ff2ec0

--- src/components/website/Header.tsx
+++ src/components/website/Header.tsx
@@ -61,7 +61,7 @@ export function Header() {
                 className="h-14 w-auto object-contain"
               />
             </div>
-            <span className="mt-[-4px] hidden lg:block pl-4 text-sm font-semibold italic tracking-wide text-primary">
+            <span className="mt-[-4px] hidden lg:block text-sm font-semibold italic tracking-wide text-primary">
               Engineering Excellence, Diagnostic Precision
             </span>
           </a>
